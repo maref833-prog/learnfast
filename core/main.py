3a04d12642460d1902006400de3d26b13eef5439
Aref <maref833@gmail.com>
2026-05-30 23:58:38 +0330

    v1.1

--- core/main.py
+++ core/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI,Query
+
+from fastapi import FastAPI,Query,status,HTTPException,Path,UploadFile,File,Form
 import uvicorn
 import random
 from typing import Annotated
+from fastapi.responses import JSONResponse
+from typing import Optional
 app=FastAPI()
 #تعریف دیتابیس 
 namelists=[
@@ -19,7 +22,7 @@ namelists=[
 def root():
     return {'message':'hello world'}
 #نمایش عناصر دیتابیس
-@app.get("/name")
+@app.get("/name",status_code=200)
 def resive():
   return namelists
 #جستجو بر اساس id
@@ -28,10 +31,10 @@ def name_idd(name_id:int):
     for name in namelists:
         if name["id"] == name_id:
             return name
- 
-    return {"input":name_id}
+    return JSONResponse(content="mofagh",status_code=200)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="peid nashod")
 #جستجو بر اساس name
-@app.get("/name/user/{name_name}")
+@app.get("/name/user/{name_name}",status_code=200)
 def r(name_name:str):
     for name in namelists:
         if name['name'] ==name_name:
@@ -39,28 +42,46 @@ def r(name_name:str):
         
     return {"input":name_name}
 #اضافه کردن
-@app.post("/name")
+@app.post("/name",status_code=201)
 def add(namea:str):
   n={'id':random.randint(8,100),'name':namea}
   namelists.append(n)
   return {"result":namea}
 #آپدیت
-@app.put("/name/{name_id}")
+@app.put("/name/{name_id}",status_code=200)
 def update(name_id:int,name:str):
  for items in namelists:
     if items['id']==name_id:
        items['name']=name
        return items
 #پاک کردن
-@app.delete("/name/{name_id}")
+@app.delete("/name/{name_id}",status_code=204)
 def dele(name_id:int):
  for items in namelists:
     if items['id']==name_id:
        namelists.remove(items)
        return {"deatel:pak shod"}
- return{"deatel:peida nashod"}
-@app.get("/search")
-def re(q:str|None =Query(default=None,max_length=50)):
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="peid nashod")
+
+@app.get("/search",status_code=200)
+def re(q:str|None =Query(default=None,max_length=50,alias="update",title="update mikoned",description="anjam update")):
    if q:
       return [item for item in namelists if item['name']==q]
    return namelists
+# @app.post("/upload/")
+# async def upload_file(file:bytes=File(...)):
+#  return {"file size":len(file)}
+
+
+
+
+
+
+
+@app.post("/files/")
+async def create_file(
+    file: Annotated[bytes, File()],
+):
+    return {
+        "file_size": len(file),
+    }
